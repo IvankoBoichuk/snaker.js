55f14acb3237dc1f1f0cb836ccc007418e12290c
Ivan Boichuk <ivan.boychuck@gmail.com>
2024-11-13 13:50:18 +0200

    change base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: 'demo',  // Вкажіть папку з index.html
-    build: {
-        outDir: '../dist',  // Вкажіть вихідну папку для збірки
-        emptyOutDir: true
-    }
+  root: 'demo',          // Коренева папка з index.html
+  base: '/snaker.js/',   // Базовий шлях, який відповідає підкаталогу розгортання
+  build: {
+    outDir: '../dist',   // Вихідна папка для збірки
+    emptyOutDir: true    // Очищає dist при кожній збірці
+  }
 });
